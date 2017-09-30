a20cd59d9b8f936734b5884fb1ca90d5a260a43f
alisa00 <mxhb01@126.com>
2017-09-30 18:30:46 +0800

    header component finished

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,25 @@ module.exports = {
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
     'semi': ['error', 'always'],
-    'indent': 0
+    //设置为0，表示忽略此规则
+    'indent': 0,
+    /** 
+     * space-before-function-paren
+     * default correct: test ()
+     * 配置忽略空格：test()
+     */
+    'space-before-function-paren': 0, // 表示 忽略函数名称后面的空格 test()
+    /**
+     * keyword-spacing：空格
+     * default correct: if () {}
+     * 配置忽略if后空格: if() {}
+     */
+    'keyword-spacing': ['error', { 
+      'overrides': {
+        'if': { 'after': false }, // if后无括号
+        'for': { 'after': false },
+        'while': { 'after': false }
+      } 
+    }]
   }
 }
