a61fd2d9eb2f07448ad373aca2ba572c863ba20a
alisa00 <mxhb01@126.com>
2017-10-10 18:34:29 +0800

    add food info

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -16,11 +16,17 @@
         <div class="price">
           <span class="now">￥{{food.price}}</span><span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
         </div>
+        <div class="cartcontrol-wrapper">
+          <cartcontrol :food="food"></cartcontrol>
+        </div>
+        <div class="buy" v-show="!food.count || food.count===0" @click.stop.prevent="addFirst" transition="fade">加入购物车</div>
       </div>
-      <div class="cartcontrol-wrapper">
-        <cartcontrol :food="food"></cartcontrol>
+      <split v-show="food.info"></split>
+      <div class="info" v-show="food.info">
+        <h1 class="title">商品介绍</h1>
+        <p class="text">{{food.info}}</p>
       </div>
-      <div class="buy" v-show="!food.count || food.count===0" @click.stop.prevent="addFirst" transition="fade">加入购物车</div>
+
     </div>
   </div>
 </template>
@@ -29,6 +35,7 @@
   import Vue from 'vue';
   import BScroll from 'better-scroll';
   import cartcontrol from 'components/cartcontrol/cartcontrol';
+  import split from 'components/split/split';
 
   export default {
     props: {
@@ -37,7 +44,8 @@
       }
     },
     components: {
-      cartcontrol
+      cartcontrol,
+      split
     },
     data() {
       return {
@@ -85,57 +93,56 @@
       transform translate3d(0, 0, 0)
     &.move-enter, &.move-leave
       transform translate3d(100%, 0, 0)
-    .food-content
+    .image-header
       position relative
-      .image-header
-        position relative
+      width 100%
+      height 0
+      padding-top 100%
+      img 
+        position absolute
+        top 0
+        left 0
         width 100%
-        height 0
-        padding-top 100%
-        img 
-          position absolute
-          top 0
-          left 0
-          width 100%
-          height 100%
-        .back
-          position absolute
-          top 10px
-          left 0
-          .icon-arrow_lift
-            display block
-            padding 10px
-            font-size 20px
-            color #ffffff
-      .content
-        padding 18px
-        .title
-          margin-bottom 8px
-          line-height 14px
+        height 100%
+      .back
+        position absolute
+        top 10px
+        left 0
+        .icon-arrow_lift
+          display block
+          padding 10px
+          font-size 20px
+          color #ffffff
+    .content
+      position relative
+      padding 18px
+      .title
+        margin-bottom 8px
+        line-height 14px
+        font-size 14px
+        font-weight 700
+        color rgb(7, 17, 27)
+      .detail
+        margin-bottom 18px
+        line-height 10px
+        height 10px
+        font-size 0
+        .sell-count, .rating
+          font-size 10px
+          color rgb(147, 153, 159)
+        .sell-count
+          margin-right 12px
+      .price
+        font-weight 400
+        line-height 24px
+        .now
+          margin-right 8px
           font-size 14px
-          font-weight 700
-          color rgb(7, 17, 27)
-        .detail
-          margin-bottom 18px
-          line-height 10px
-          height 10px
-          font-size 0
-          .sell-count, .rating
-            font-size 10px
-            color rgb(147, 153, 159)
-          .sell-count
-            margin-right 12px
-        .price
-          font-weight 400
-          line-height 24px
-          .now
-            margin-right 8px
-            font-size 14px
-            color rgb(240, 20, 20)
-          .old
-            text-decoration line-through
-            font-size 10px
-            color rgb(147, 153, 159)
+          color rgb(240, 20, 20)
+        .old
+          text-decoration line-through
+          font-size 10px
+          color rgb(147, 153, 159)
       .cartcontrol-wrapper
         position absolute
         right 12px
@@ -158,6 +165,19 @@
           opacity 1
         &.fade-enter,&.fade-leave
           opacity 0
+    .info
+      padding 18px
+      .title
+        line-height 14px
+        margin-bottom 6px
+        font-size 14px
+        color rgb(7, 17, 27)
+      .text
+        padding 0 8px
+        line-height 24px
+        font-size 12px
+        color rgb(77, 85, 93)
+        
 
 </style>
 
